9b0636575321bc9fcb1d94c92bc16321de429c90
Jeseel Albuquerque <jeseel.albuquerque@gmail.com>
2018-06-11 09:53:38 -0400

    material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {CustomMaterialModule} from './custom-material/custom-material.module';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    DashboardComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomMaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
